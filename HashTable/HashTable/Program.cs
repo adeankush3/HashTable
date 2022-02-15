4ad808708bbe9bbf7aa1200ecd7effbb826e21f1
adeankush3 <adeankush3@gmail.com>
2022-02-15 13:07:41 +0530

    [Ankush]Ability to find Frequency Of Word in Large Complete

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -26,6 +26,31 @@ namespace HashTable
             myMapNode.Remove("2");
             string hash2 = myMapNode.Get("2");
             Console.WriteLine("2nd index Value " + hash2);
+
+            MyMapNode<string, string> hashTable = new MyMapNode<string, string>(18);
+
+            hashTable.Add("f0", "“Paranoids");
+            hashTable.Add("f1", "are");
+            hashTable.Add("f2", "but");
+            hashTable.Add("f3", "paranoid");
+            hashTable.Add("f4", "because");
+            hashTable.Add("f5", "they");
+            hashTable.Add("f6", "are");
+            hashTable.Add("f7", "paranoid");
+            hashTable.Add("f8", "but");
+            hashTable.Add("f9", "because");
+            hashTable.Add("f10", "they");
+            hashTable.Add("f11", "keep");
+            hashTable.Add("f12", "putting");
+            hashTable.Add("f13", "themselves");
+            hashTable.Add("f14", "deliberately");
+            hashTable.Add("f15", "into");
+            hashTable.Add("f16", "paranoid");
+            hashTable.Add("f17", "avoidable");
+            hashTable.Add("f18", "situations”");
+
+            string strHold = hashTable.Get("f8");
+            System.Console.WriteLine("8th index word in the paragraph is  :" + strHold);
         }
     }
 }
